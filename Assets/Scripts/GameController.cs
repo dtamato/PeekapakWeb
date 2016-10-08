1e083265d1998f79eef0935a65858c1ba45b2ddc
Rebecca <rebeccamitchell88@gmail.com>
2016-10-08 18:20:22 -0400

    Push base for emotional journey game.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public enum GradeLevel { PreK, Kindergarten, Grade1, Grade2, Grade3, };
+
+    [SerializeField] Sprite[] faceSprites;
+    [SerializeField] GameObject[] faceSpots;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
