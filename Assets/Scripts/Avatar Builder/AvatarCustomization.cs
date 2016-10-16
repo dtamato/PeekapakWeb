d9d5aff79932b152cee3951544c4366f69ff063b
Dave Tamayo <djbtamayo@gmail.com>
2016-10-15 23:05:03 -0400

    Worked on the Avatar Builder some more. Imported in some branding material.

---
+++ Assets/Scripts/Avatar Builder/AvatarCustomization.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[DisallowMultipleComponent]
+public class AvatarCustomization : MonoBehaviour {
+
+    [Header("Avatar Preview Images")]
+    [SerializeField] Image shoesImage;
+
+    [Header("Grids")]
+    [SerializeField] GameObject skinGrid;
+    [SerializeField] GameObject hairGrid;
+    [SerializeField] GameObject eyesGrid;
+    [SerializeField] GameObject topGrid;
+    [SerializeField] GameObject bottomGrid;
+    [SerializeField] GameObject shoesGrid; 
+
+	
+    void OnEnable () {
+        
+        HideAllGrids();
+    }
+
+    void HideAllGrids () {
+
+        skinGrid.SetActive(false);
+        hairGrid.SetActive(false);
+        eyesGrid.SetActive(false);
+        topGrid.SetActive(false);
+        bottomGrid.SetActive(false);
+        shoesGrid.SetActive(false);
+    }
+
+    public void ToggleGrid (GameObject gridToToggle) {
+
+        if (gridToToggle.activeSelf) {
+
+            gridToToggle.SetActive(false);
+        }
+        else {
+
+            HideAllGrids();
+            gridToToggle.SetActive(!gridToToggle.activeSelf);
+        }
+    }
+
+    public void UpdateShoes () {
+
+        Sprite newShoeSprite = EventSystem.current.currentSelectedGameObject.GetComponent<Image>().sprite;
+
+        if(newShoeSprite) {
+
+            shoesImage.sprite = newShoeSprite;
+        }
+    }
+}
