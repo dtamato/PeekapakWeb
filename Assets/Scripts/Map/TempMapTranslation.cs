f4415171f61a114a5889928538d3f9ff1fbb9808
Dave Tamayo <djbtamayo@gmail.com>
2016-11-16 19:38:20 -0500

    Updated map scene to the new map that Brittany made Updated the Navigator scene to be able to navigate to other scenes

---
+++ Assets/Scripts/Map/TempMapTranslation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class TempMapTranslation : MonoBehaviour {
+
+	[SerializeField] float moveSpeed = 1;
+	[SerializeField] float leftBoundary;
+	[SerializeField] float rightBoundary;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (Input.GetAxis ("Horizontal") != 0) {
+
+			this.transform.Translate (Input.GetAxisRaw ("Horizontal") * moveSpeed * Vector3.left);
+
+			float newX = this.transform.position.x;
+			newX = Mathf.Clamp (newX, leftBoundary, rightBoundary);
+			this.transform.position = new Vector3 (newX, 0, 0);
+		}
+	}
+}
