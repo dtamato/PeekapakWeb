ec71ce9bb7c9bdb8ef702db3f693859501c416d3
Rebecca <rebeccamitchell88@gmail.com>
2016-11-22 17:39:04 -0500

    continued work on the Emotional Match Game 2.0

--- Assets/Scripts/New Emotional Match Game/EmotionalMatchGameController.cs
+++ Assets/Scripts/New Emotional Match Game/EmotionalMatchGameController.cs
@@ -11,23 +11,41 @@ public class EmotionalMatchGameController : MonoBehaviour {
     [Header("Lucia's House")]
     [SerializeField] Sprite luciasHouse_panelImage;
     [SerializeField] string luciasHouse_scenario;
-    [SerializeField] string[] luciasHouse_answers;
+    [SerializeField] string luciasHouse_rightAnswerResponse;
+    [SerializeField] string luciasHouse_wrongAnswerResponse;
+    [SerializeField] Sprite[] luciasHouse_answersImages;
+    [SerializeField] string[] luciasHouse_answersText;
+
+    [Header("School")]
+    [SerializeField] Sprite school_panelImage;
+    [SerializeField] string school_scenario;
+    [SerializeField] string school_rightAnswerResponse;
+    [SerializeField] string school_wrongAnswerResponse;
+    [SerializeField] string[] school_answersText;
+
+    [Header("Lucia's House")]
+    [SerializeField] Sprite tree_panelImage;
+    [SerializeField] string tree_scenario;
+    [SerializeField] string tree_rightAnswerResponse;
+    [SerializeField] string tree_wrongAnswerResponse;
+    [SerializeField] Sprite[] tree_answersImages;
+    [SerializeField] string[] tree_answersText;
 
     Sprite panelImage;
     string scenario;
+    string rightAnswerResponse;
+    string wrongAnswerResponse;
     int[] answers = {1, 0, 0};
     string[] answersText = new string[3];
     Sprite[] answersImages = new Sprite[3];
 
 	// Use this for initialization
 	void Start () {
-        currentLocation = Location.LUCIAS_HOUSE;
+        currentLocation = Location.TREE;
         uiController = GameObject.FindGameObjectWithTag("UIController").GetComponent<EmotionalMatchUIController>();
         CheckLocation();
     }
 	
-    
-
 	// Update is called once per frame
 	void Update () {
 	
@@ -36,19 +54,28 @@ public class EmotionalMatchGameController : MonoBehaviour {
     void CheckLocation() {
         switch (currentLocation) {
             case Location.LUCIAS_HOUSE:
-                LoadWordGame(luciasHouse_answers, luciasHouse_panelImage, luciasHouse_scenario);
+                LoadGame(luciasHouse_panelImage, luciasHouse_scenario, luciasHouse_rightAnswerResponse, luciasHouse_wrongAnswerResponse);
+                LoadImageGame(luciasHouse_answersImages, luciasHouse_answersText);
                 break;
             case Location.SCHOOL:
+                LoadGame(school_panelImage, school_scenario, school_rightAnswerResponse, school_wrongAnswerResponse);
+                LoadWordGame(school_answersText);
                 break;
             case Location.TREE:
+                LoadGame(tree_panelImage, tree_scenario, tree_rightAnswerResponse, tree_wrongAnswerResponse);
+                LoadImageGame(tree_answersImages, tree_answersText);
                 break;
         }
     }
 
-    void LoadWordGame(string[] newAnswersText, Sprite newPanelImage, string newScenario) {
-        // Fill the current game variables
+    void LoadGame(Sprite newPanelImage, string newScenario, string newRightAnswer, string newWrongAnswer) {
         scenario = newScenario;
         panelImage = newPanelImage;
+        rightAnswerResponse = newRightAnswer;
+        wrongAnswerResponse = newWrongAnswer;
+    }
+
+    void LoadWordGame(string[] newAnswersText) {
         for (int i = 0; i < newAnswersText.Length; i++) {
             answersText[i] = newAnswersText[i];
         }
@@ -56,20 +83,18 @@ public class EmotionalMatchGameController : MonoBehaviour {
         // Shuffle the right answers and their corresponding 
         ShuffleTextAnswers();
         // Send those results to the UI Controller so it can apply the tags
-
+        uiController.LoadWordGame(answersText, answers, panelImage, scenario, rightAnswerResponse, wrongAnswerResponse);
     }
 
-    void LoadTextGame(Sprite[] newAnswersImages, Sprite newPanelImage, string newScenario) {
-        // Fill the current game variables
-        scenario = newScenario;
-        panelImage = newPanelImage;
+    void LoadImageGame(Sprite[] newAnswersImages, string[] newAnswersText) {
         for (int i = 0; i < newAnswersImages.Length; i++) {
             answersImages[i] = newAnswersImages[i];
+            answersText[i] = newAnswersText[i];
         }
         // Shuffle the right answers and their corresponding 
         ShuffleImageAnswers();
         // Send those results to the UI Controller so it can apply the tags
-
+        uiController.LoadImageGame(answersImages, answersText, answers, panelImage, scenario, rightAnswerResponse, wrongAnswerResponse);
     }
 
     void ShuffleTextAnswers() {
@@ -90,6 +115,7 @@ public class EmotionalMatchGameController : MonoBehaviour {
     void ShuffleImageAnswers() {
         for (int i = 0; i < answers.Length; i++) {
             Sprite tempImage = answersImages[i];
+            string tempString = answersText[i];
             int tempInt = answers[i];
             // Get a random value
             int rand = Random.Range(0, answers.Length);
@@ -99,14 +125,20 @@ public class EmotionalMatchGameController : MonoBehaviour {
             // Set the new int values
             answersImages[i] = answersImages[rand];
             answersImages[rand] = tempImage;
+            // Set the new String Values
+            answersText[i] = answersText[rand];
+            answersText[rand] = tempString;
         }
     }
 
     public void CheckAnswer(GameObject answer) {
         if (answer.CompareTag("Right Answer")) {
+            Debug.Log("right Answer");
+            uiController.RightAnswer();
 
         } else if (answer.CompareTag("Wrong Answer")) {
-
+            Debug.Log("wrong Answer");
+            uiController.WrongAnswer();
         }
     }
 }
--- Assets/Scripts/New Emotional Match Game/EmotionalMatchUIController.cs
+++ Assets/Scripts/New Emotional Match Game/EmotionalMatchUIController.cs
@@ -6,11 +6,17 @@ public class EmotionalMatchUIController : MonoBehaviour {
 
     [SerializeField] GameObject rightAnswerPanel;
     [SerializeField] GameObject wrongAnswerPanel;
+    [SerializeField] GameObject textAnswersPanel;
+    [SerializeField] GameObject textAndImageAnswersPanel;
     [SerializeField] GameObject gameBoard;
 
-    [SerializeField] Text[] answers;
+    [SerializeField] Text[] textAnswers;
+    [SerializeField] Image[] imageAnswers;
+    [SerializeField] Text[] imageDefinitions;
     [SerializeField] Image panelImage;
     [SerializeField] Text scenarioText;
+    [SerializeField] Text wrongAnswerRespose;
+    [SerializeField] Text rightAnswerRespose;
 
 	// Use this for initialization
 	void Start () {
@@ -22,15 +28,50 @@ public class EmotionalMatchUIController : MonoBehaviour {
 	
 	}
 
-    public void LoadWordGame(string[] newAnswers, int[] answerValues, Sprite naePanelImage, Text newScenario) {
-
+    void LoadGame(Sprite newPanelImage, string newScenario, string newRightAnswer, string newWrongAnswer) {
+        // Set the Panel Image
+        panelImage.sprite = newPanelImage;
+        // Set the Scenario Text
+        scenarioText.text = newScenario;
+        // Set the wrong and right answer responses
+        rightAnswerRespose.text = newRightAnswer;
+        wrongAnswerRespose.text = newWrongAnswer;
     }
 
-    public void LoadImageGame(Sprite[] newAnswers, int[] answerValues, Sprite naePanelImage, Text newScenario) {
+    public void LoadWordGame(string[] newAnswers, int[] answerValues, Sprite newPanelImage, string newScenario, string newRightAnswer, string newWrongAnswer) {
+        // Make sure the right answer options are toggled on
+        textAnswersPanel.SetActive(true);
+        textAndImageAnswersPanel.SetActive(false);
+        // Fill the words
+        for (int i = 0; i < newAnswers.Length; i++) {
+            textAnswers[i].text = newAnswers[i];
+            if (answerValues[i] == 1) { textAnswers[i].transform.parent.gameObject.tag = "Right Answer"; }
+            else { textAnswers[i].transform.parent.gameObject.tag = "Wrong Answer"; }
+        }
 
+        LoadGame(newPanelImage, newScenario, newRightAnswer, newWrongAnswer);
     }
 
-    public void RightAAnswer() {
+    public void LoadImageGame(Sprite[] newAnswerImages, string[] newAnswersText, int[] answerValues, Sprite newPanelImage, string newScenario, string newRightAnswer, string newWrongAnswer) {
+        Debug.Log("Loading Game Visuals");
+        // Make sure the right answer options are toggled on
+        textAnswersPanel.SetActive(false);
+        textAndImageAnswersPanel.SetActive(true);
+        // Fill the Images and set them to maintain aspect ratio
+        for (int i = 0; i < newAnswerImages.Length; i++) {
+            imageDefinitions[i].text = newAnswersText[i];
+            imageAnswers[i].sprite = newAnswerImages[i];
+            imageAnswers[i].preserveAspect = true;
+            // If this is the right answer
+            if (answerValues[i] == 1) { imageAnswers[i].transform.parent.gameObject.tag = "Right Answer"; }
+            // If it is the worng answer
+            else { imageAnswers[i].transform.parent.gameObject.tag = "Wrong Answer"; }
+        }
+
+        LoadGame(newPanelImage, newScenario, newRightAnswer, newWrongAnswer);
+    }
+
+    public void RightAnswer() {
         // gameBoard.SetActive(false);
         rightAnswerPanel.SetActive(true);
     }
