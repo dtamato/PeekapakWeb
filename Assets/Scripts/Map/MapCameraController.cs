599118cbabea9dcb17acd4a45151f83843c1933d
Dave Tamayo <djbtamayo@gmail.com>
2016-12-04 21:07:00 -0500

    Continued work on the Map scene

---
+++ Assets/Scripts/Map/MapCameraController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class MapCameraController : MonoBehaviour {
+
+	[Header("Movement Parameters")]
+	[SerializeField] float moveSpeed = 1;
+	[SerializeField] float leftBoundary = -250;
+	[SerializeField] float rightBoundary = 6683;
+
+	[Header("References")]
+	[SerializeField] GameObject playerHUD;
+
+	bool canMove = true;
+
+	void Awake () {
+	
+		this.transform.position = new Vector3 (leftBoundary, this.transform.position.y, this.transform.position.z);
+	}
+
+	void Update () {
+	
+		if (canMove && Input.GetAxis ("Horizontal") != 0) {
+
+			this.transform.Translate (Input.GetAxisRaw ("Horizontal") * moveSpeed * Vector3.right);
+
+			float newX = this.transform.position.x;
+			newX = Mathf.Clamp (newX, leftBoundary, rightBoundary);
+			this.transform.position = new Vector3 (newX, this.transform.position.y, this.transform.position.z);
+		}
+	}
+
+	IEnumerator ZoomCamera (float newSize) {
+
+		if(newSize < Camera.main.orthographicSize) { playerHUD.SetActive(false); }
+
+		while(Mathf.Abs(Camera.main.orthographicSize - newSize) > 0.5f) {
+
+			Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, newSize, 2 * Time.deltaTime);
+			yield return null;
+		}
+
+		if(newSize > Camera.main.orthographicSize) { playerHUD.SetActive(true); }
+		Camera.main.orthographicSize = newSize;
+		canMove = !canMove;
+		StopAllCoroutines();
+	}
+
+	IEnumerator MovePosition (Vector3 newPosition) {
+
+		while(Vector3.Distance(Camera.main.transform.position, newPosition) > 0.5f) {
+
+			Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, newPosition, 2 * Time.deltaTime);
+			yield return null;
+		}
+
+		Camera.main.transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/Map/MapCharacter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class MapCharacter : MonoBehaviour {
+
+	[Header("Camera Targets")]
+	[SerializeField] float targetCameraSize;
+	[SerializeField] Vector3 targetCameraPosition;
+
+	[Header("References")]
+	[SerializeField] GameObject speechBubble;
+
+	MapCameraController cameraController;
+	float initialCameraSize;
+	Vector3 initialCameraPosition;
+
+	void Awake () {
+
+		// Initialize
+		speechBubble.SetActive(false);
+		cameraController = Camera.main.GetComponent<MapCameraController>();
+	}
+
+	public void StartConversation () {
+
+		// Save camera data
+		initialCameraSize = Camera.main.orthographicSize;
+		initialCameraPosition = Camera.main.transform.position;
+
+		this.GetComponent<Button>().interactable = false;
+		speechBubble.SetActive(true);
+
+		cameraController.StopAllCoroutines();
+		cameraController.StartCoroutine("ZoomCamera", targetCameraSize);
+		cameraController.StartCoroutine("MovePosition", targetCameraPosition);
+	}
+
+	public void LeaveConversation () {
+
+		this.GetComponent<Button>().interactable = true;
+		speechBubble.SetActive(false);
+
+		cameraController.StopAllCoroutines();
+		cameraController.StartCoroutine("ZoomCamera", initialCameraSize);
+		cameraController.StartCoroutine("MovePosition", initialCameraPosition);
+	}
+}
