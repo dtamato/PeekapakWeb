96c02332103013e3bf41fa6fd94daa78f4274de8
Dave Tamayo <djbtamayo@gmail.com>
2016-12-27 21:49:56 -0500

    Tied the interior of the ice cream parlor to the map Added the new tree of gratitude sprite Limited the size of some of the big map sprites Started working on the functionality for Leo in the map Implemented what I could for the eyes in the Avatar Builder

---
+++ Assets/Scripts/Map/IceCreamParlor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IceCreamParlor : MonoBehaviour {
+
+	[SerializeField] GameObject parlorInterior;
+	[SerializeField] GameObject textPanel;
+
+	bool isUnlocked = false;
+
+
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Q)) {
+
+			isUnlocked = true;
+		}
+	}
+
+	public void OpenParlor () {
+
+		if(isUnlocked) {
+
+			parlorInterior.SetActive(true);
+		}
+		else {
+
+			textPanel.SetActive(true);
+			textPanel.GetComponentInChildren<Text>().text = "Seems to be closed...";
+		}
+	}
+}
