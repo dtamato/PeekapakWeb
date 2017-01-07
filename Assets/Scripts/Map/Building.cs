92722094c30968a8e1e84c9d8ba08f7fc3d7f568
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2017-01-06 23:19:06 -0500

    Working on the map - I think i had to re do some of the stuff you did dave, sorry, i didn't pull.

---
+++ Assets/Scripts/Map/Building.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+    public enum LocationType { EMOTIONAL_JOURNEY, EMOTIONAL_MATCH };
+    [SerializeField] LocationType locationType;
+
+	[SerializeField] GameObject interior;
+	[SerializeField] GameObject textPanel;
+    [SerializeField] string destinationName;
+
+	bool isUnlocked = false;
+
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Q)) {
+
+			isUnlocked = true;
+		}
+	}
+
+	public void OpenBuilding () {
+
+		if(isUnlocked) {
+
+			interior.SetActive(true);
+		}
+		else {
+
+			textPanel.SetActive(true);
+			textPanel.GetComponentInChildren<Text>().text = "Seems to be closed...";
+		}
+	}
+}
