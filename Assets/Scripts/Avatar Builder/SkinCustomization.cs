8d6f9de9ae8e66018783ba81c9a45de256686578
Dave Tamayo <djbtamayo@gmail.com>
2017-01-10 20:38:28 -0500

    Polished up the Avatar Builder icons more

---
+++ Assets/Scripts/Avatar Builder/SkinCustomization.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class SkinCustomization : MonoBehaviour {
+
+	[SerializeField] Image avatarHeadPaint;
+	[SerializeField] Image avatarBodyPaint;
+
+
+	public void ChangeSkinColor () {
+
+		avatarHeadPaint.color = EventSystem.current.currentSelectedGameObject.GetComponent<Image> ().color;
+		avatarBodyPaint.color = EventSystem.current.currentSelectedGameObject.GetComponent<Image> ().color;
+	}
+}
