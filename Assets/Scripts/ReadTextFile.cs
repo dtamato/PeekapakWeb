aaf209bd766f2757df977fb189e0c50cb46465a0
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2017-01-25 12:18:20 -0500

    Text reader Assets

---
+++ Assets/Scripts/ReadTextFile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadTextFile : MonoBehaviour {
+
+    [SerializeField] TextAsset testTA;
+
+    string[] linesInFile;
+
+	// Use this for initialization
+	void Start () {
+        // Read in and break for every new line
+        linesInFile = testTA.text.Split('\n');
+
+        foreach (string line in linesInFile) {
+            Debug.Log(line);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
